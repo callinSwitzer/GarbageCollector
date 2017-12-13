64dc6d7ce7e9316714e1c124ebb288ef8672be55
callinSwitzer <callin.switzer@gmail.com>
2017-12-12 16:56:52 -0800

    updated env mon

--- EnvironmentalMonitoring.R
+++ EnvironmentalMonitoring.R
@@ -18,15 +18,16 @@ library(lubridate)
 bb = aa - hours(4) 
 
 
-car::scatterplotMatrix(dd)
-
 
 dd["temp.F."] = dd$temp * 9/5 + 32
 
-par(mfrow = c(3,2))
+dd$light <- 1024 - dd$light
+quartz()
+par(mfrow = c(5,1))
+par(mai = c(0.4, 0.5, 0.2, 0.5))
 for(ii in colnames(dd)[2:6]){
   #plot(x = bb, y = dd[,ii], main = ii, ylab = ii,  xlab = "time", pch = ".", type = "l")
-  preds = predict(loess(dd[,ii] ~ as.numeric(bb), span = 0.05))
+  preds = predict(loess(dd[,ii] ~ as.numeric(bb), span = 0.005))
   plot(x = bb, y = preds, main = ii, ylab = ii,  xlab = "time", pch = ".", type = "l")
   
 }
