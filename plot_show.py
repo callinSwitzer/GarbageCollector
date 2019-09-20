1913c95405e9daa08eab1aba21f2e5c4d961eb9f
Callin Switzer <callin.switzer@gmail.com>
2019-09-20 09:51:01 -0700

    updated st. ex. answer

---
+++ plot_show.py
@@ -0,0 +1,11 @@
+
+
+import matplotlib
+matplotlib.use("TkAgg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+print("hello")
+plt.plot(np.random.rand(10), np.random.rand(10))
+plt.show()
+
